53508d7ec6385150c10ad6e816aedf48aeb0c355
Aleksandr-Abramov <ale19850505@gmail.com>
2021-01-13 21:31:44 +0300

    yatube sprint6

---
+++ yatube/posts/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # гвавная страница
+    path("", views.index, name="index"),
+    # страница группы
+    path("group/<slug:slug>/", views.group_posts, name="group_post"),
+    #страница с формой добовления поста
+    path("new/", views.new_post, name="new_post"),
+    # Профайл пользователя
+    path('<str:username>/', views.profile, name='profile'),
+    # Просмотр записи
+    path('<str:username>/<int:post_id>/', views.post_view, name='post'),
+    # Страница редактирования поста
+    path('<str:username>/<int:post_id>/edit/', views.post_edit, name='post_edit'),
+    path('404/', views.page_not_found, name="page404"),
+    path('500/', views.server_error, name="page500")
+]
